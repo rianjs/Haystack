1a3fb4ebace8f8235cdaab425c96bba8927b7547
Rian Stockbower <rstockbower@gmail.com>
2020-06-06 16:45:56 -0400

    Add some compression and decompression utility methods that make it easy to compress and decompress byte arrays. Supports gzip and DEFLATE

---
+++ Haystack/IO/Compression.cs
@@ -0,0 +1,140 @@
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace Haystack.IO
+{
+    /// <summary>
+    /// Extension methods for the System.IO.Compression namespace
+    /// </summary>
+    public static class Compression
+    {
+        /// <summary>
+        /// Compresses a byte array using Gzip compression.
+        /// </summary>
+        public static byte[] CompressGzip(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var compressor = new GZipStream(destination, CompressionMode.Compress))
+                {
+                    source.CopyTo(compressor);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Decompresses a byte array that has been compressed using Gzip compression.
+        /// </summary>
+        public static byte[] DecompressGzip(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var decompressor = new GZipStream(source, CompressionMode.Decompress))
+                {
+                    decompressor.CopyTo(destination);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Compresses a byte array using Gzip compression. Does not block threads while waiting for IO to complete.
+        /// </summary>
+        public static async Task<byte[]> CompressGzipAsync(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var compressor = new GZipStream(destination, CompressionMode.Compress))
+                {
+                    await source.CopyToAsync(compressor);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Decompresses a byte array that has been compressed using Gzip compression. Does not block threads while waiting for IO to complete.
+        /// </summary>
+        public static async Task<byte[]> DecompressGzipAsync(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var decompressor = new GZipStream(source, CompressionMode.Decompress))
+                {
+                    await decompressor.CopyToAsync(destination);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Compresses a byte array using DEFLATE compression.
+        /// </summary>
+        public static byte[] CompressDeflate(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var compressor = new DeflateStream(destination, CompressionMode.Compress))
+                {
+                    source.CopyTo(compressor);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Compresses a byte array using DEFLATE compression. Does not block threads while waiting for IO to complete.
+        /// </summary>
+        public static async Task<byte[]> CompressDeflateAsync(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var compressor = new DeflateStream(destination, CompressionMode.Compress))
+                {
+                    await source.CopyToAsync(compressor);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Decompresses a byte array that has been compressed using DEFLATE compression.
+        /// </summary>
+        public static byte[] DecompressDeflate(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var decompressor = new DeflateStream(source, CompressionMode.Decompress))
+                {
+                    decompressor.CopyTo(destination);
+                }
+                return destination.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Decompresses a byte array that has been compressed using DEFLATE compression. Does not block threads while waiting for IO to complete.
+        /// </summary>
+        public static async Task<byte[]> DecompressDeflateAsync(this byte[] b)
+        {
+            using (var source = new MemoryStream(b))
+            using (var destination = new MemoryStream())
+            {
+                using (var decompressor = new DeflateStream(source, CompressionMode.Decompress))
+                {
+                    await decompressor.CopyToAsync(destination);
+                }
+                return destination.ToArray();
+            }
+        }
+    }
+}
---
+++ UnitTests/IO/CompressionTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Haystack.IO;
+using NUnit.Framework;
+
+namespace UnitTests.IO
+{
+    public class CompressionTests
+    {
+        private static readonly string _someText = DateTime.Now.ToString("F");
+        private static readonly byte[] _someBytes = Encoding.UTF8.GetBytes(_someText);
+        
+        [Test]
+        public void TestCompressionSymmetry()
+        {
+            var gz = _someBytes.CompressGzip();
+            var decompGz = gz.DecompressGzip();
+            var decompGzString = Encoding.UTF8.GetString(decompGz);
+
+            var deflate = _someBytes.CompressDeflate();
+            var decompDeflate = deflate.DecompressDeflate();
+            var decompDeflateString = Encoding.UTF8.GetString(decompDeflate);
+            
+            Assert.AreEqual(decompDeflateString, decompGzString);
+            Assert.AreEqual(decompGzString, _someText);
+        }
+
+        [Test]
+        public async Task TestCompressionSymmetryAsync()
+        {
+            var gz = await _someBytes.CompressGzipAsync();
+            var decompGz = await gz.DecompressGzipAsync();
+            var decompGzString = Encoding.UTF8.GetString(decompGz);
+
+            var deflate = await _someBytes.CompressDeflateAsync();
+            var decompDeflate = await deflate.DecompressDeflateAsync();
+            var decompDeflateString = Encoding.UTF8.GetString(decompDeflate);
+
+            Assert.AreEqual(decompDeflateString, decompGzString);
+            Assert.AreEqual(decompGzString, _someText);
+        }
+    }
+}
